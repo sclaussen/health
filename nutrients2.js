af6853fa5d449b2dcb701b1eafa7d0db22633f18
Shane Claussen <sclaussen@yahoo.com>
2022-06-09 17:55:20 -0700

    updates

--- nutrients2.js
+++ nutrients2.js
@@ -1,290 +1,231 @@
 'use strict'
-// process.env.DEBUG = 'health';
-const d = require('debug')('health');
-const p = require('./lib/pr').p(d);
-const p4 = require('./lib/pr').p4(d);
+process.env.DEBUG = 'health'
+const fs = require('fs')
+var YAML = require('js-yaml');
+
+const d = require('debug')('health')
+const p = require('./lib/pr').p(d)
+const p4 = require('./lib/pr').p4(d)
 
 const _ = require('lodash')
-const curl = require('./lib/curl');
-const table = require('./lib/table').table;
+const curl = require('./lib/curl')
+const table = require('./lib/table').table
 
-var ingredients = [
-    // lettuce red leaf
-    // Siggis, Yogurt Plain Whole Milk
-    // Nature’s Rancher, No Sugar Added Hickory Smoked Bacon, 12 ozNature’s Rancher, No Sugar Added Hickory Smoked Baco
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-coconut-oil-expeller-expressed-14-fl-oz-b074h5bv9y',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-extra-virgin-olive-oil-cold-processed-mediterranean-blend-1014-fl-oz-b074y6wz8x',
-    'https://www.wholefoodsmarket.com/product/produce-serrano-pepper-b0787w921k',
-    'https://www.wholefoodsmarket.com/product/vital-farms-organic-large-eggs-36-oz-b089c8jt7b',
-    'https://www.wholefoodsmarket.com/product/vital-farms-pastureraised-alfresco-eggs-24-oz-b0849mz45y',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-broccoli-florets-16-oz-b0812lws2s',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-frozen-vegetables-broccoli-florets-b074h5b5h7',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-cauliflower-florets-16-oz-b0812kzpj4',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-produce-organic-packaged-baby-arugula-b07pff27d1',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-produce-organic-packaged-baby-spinach-b082fjnkjv',
-    'https://www.wholefoodsmarket.com/product/produce-organic-romaine-lettuce-b07812n26q',
-    'https://www.wholefoodsmarket.com/product/produce-organic-collard-green-b0785vryvh',
-    'https://www.wholefoodsmarket.com/product/produce-organic-white-mushroom-b07814mfc7',
-    'https://www.wholefoodsmarket.com/product/produce-organic-red-radish-bunch-b09qqlf423',
-    'https://www.wholefoodsmarket.com/product/calavo-bagged-hass-avocados-b07fxsz49r',
-    'https://www.wholefoodsmarket.com/product/organicville-organic-stone-ground-mustard-12-oz-b00mfnax52',
-    'https://www.wholefoodsmarket.com/product/wild-planet-wild-sardines-in-extra-virgin-olive-oil-b00xmzo3qm',
-    'https://www.wholefoodsmarket.com/product/wild-planet-skinless-boneless-wild-sardine-fillets-in-olive-oil-425-oz-b07w7vcj1v',
-    'https://www.wholefoodsmarket.com/product/wild-planet-wild-mackerel-fillets-b076vw85s5',
-    'https://www.wholefoodsmarket.com/product/wild-planet-wild-albacore-tuna-5-oz-b00cq7puim',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-mozzarella-string-cheese-24pk-1-each-b074h6r58h',
-    'https://www.wholefoodsmarket.com/product/365-by-wfm-avocado-oil-254-fl-oz-b08sxszjwf',
-    'https://www.wholefoodsmarket.com/product/kerrygold-grassfed-dubliner-irish-cheese-7oz-b001e96kuu',
-    'https://www.wholefoodsmarket.com/product/bhu-foods-keto-chocolate-chip-cookie-dough-bites-529-oz-b07tt8fd4f',
-    'https://www.wholefoodsmarket.com/product/bhu-foods-keto-double-dark-chocolate-chip-cookie-dough-bites-529-oz-b07tt8fkvx',
-    'https://www.wholefoodsmarket.com/product/bhu-foods-peanut-butter-cookie-dough-bites-529-oz-b08dx5md5l',
-    'https://www.wholefoodsmarket.com/product/bhu-foods-white-chocolate-macadamia-cookie-dough-bite-088-oz-b087xq5f64',
-    'https://www.wholefoodsmarket.com/product/whole-foods-market-premium-whole-macadamia-nuts-8-oz-b002hqnkvq',
-    'https://www.wholefoodsmarket.com/product/babybel-mini-original-semisoft-cheese-6pk-45-ounce-b000qg54yu',
-    'https://www.wholefoodsmarket.com/product/365-everyday-value-organic-pumpkin-seeds-8-ounce-b07gl6jlzy',
-    'https://www.wholefoodsmarket.com/product/365-everyday-value-shredded-monterey-jack-cheddar-8-oz-b07vm4mzs6',
-    'https://www.wholefoodsmarket.com/product/produce-banana-b07fyykkqk',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-peanuts-dry-roasted-unsalted-16-oz-b07qmw5y77',
-    'https://www.wholefoodsmarket.com/product/produce-navel-orange-b000se9nug',
-    'https://www.wholefoodsmarket.com/product/produce-organic-kiwi-b077zxhwhh',
-    'https://www.wholefoodsmarket.com/product/produce-organic-fuji-apple-b000ytxxdi',
-    'https://www.wholefoodsmarket.com/product/produce-organic-carrots-b07813yjl1',
-    'https://www.wholefoodsmarket.com/product/produce-organic-red-bell-pepper-b000rgyjqi',
-    'https://www.wholefoodsmarket.com/product/produce-organic-green-bell-pepper-b000p6j14k',
-    'https://www.wholefoodsmarket.com/product/produce-organic-white-onion-b0787z3t3b',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-stone-ground-corn-tortilla-8-oz-b084r4r9pk',
-    'https://www.wholefoodsmarket.com/product/organic-valley-organic-whole-milk-grassmilk-b06xwl6tfw',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-organic-california-raisins-sundried-12-oz-b07qg4mn46',
-    'https://www.wholefoodsmarket.com/product/doctor-in-the-kitchen-organic-rosemary-flackers-5-oz-b0078dq85s',
-    'https://www.wholefoodsmarket.com/product/365-everyday-value-organic-black-pepper-cracked-turkey-breast-b074h5y183',
-    'https://www.wholefoodsmarket.com/product/produce-organic-green-cabbage-b0787tk5fv',
-    'https://www.wholefoodsmarket.com/product/food-for-life-ezekiel-49-original-bread-24-oz-b000rey4mo',
-    'https://www.wholefoodsmarket.com/product/crofters-organic-strawberry-premium-fruit-spread-165-oz-b004si9nhq',
-    'https://www.wholefoodsmarket.com/product/365-by-whole-foods-market-swiss-cheese-slices-8-oz-b07nrcgwys',
-]
 
-var macroNutrientSearchCriterias = [
-    {
-        name: 'Calories',
-        units: [ 'kcal' ]
-    },
-    {
-        name: 'Total Fat',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Saturated Fat',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Trans Fat',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Polyunsaturated Fat',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Monounsaturated Fat',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Cholesterol',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Sodium',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Carbohydrates',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Fiber',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Sugars',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Includes Added Sugars',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Sugar Alcohol',
-        units: [ 'g' ]
-    },
-    {
-        name: 'Protein',
-        units: [ 'g' ]
-    },
-]
+// Missing:
+// lettuce red leaf
+// Nature’s Rancher, No Sugar Added Hickory Smoked Bacon, 12 ozNature’s Rancher, No Sugar Added Hickory Smoked Baco
+// ...
 
-var microNutrientSearchCriterias = [
-    {
-        name: 'Zinc',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Vitamin K',
-        units: [ 'µg', 'mcg' ]
-    },
-    {
-        name: 'Vitamin E',
-        units: [ 'mg', 'IU' ],
-        conversion_factor: 1.5,
-    },
-    {
-        name: 'Vitamin D',
-        units: [ 'mcg', 'IU' ],
-        conversion_factor: 40,
-    },
-    {
-        name: 'Vitamin C',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Vitamin B6',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Vitamin B12',
-        units: [ 'µg', 'mcg' ]
-    },
-    {
-        name: 'Vitamin A',
-        units: [ 'mcg', 'IU' ],
-        conversion_factor: 3.336,
-    },
-    {
-        name: 'Thiamin',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Selenium',
-        units: [ 'µg', 'mcg' ]
-    },
-    {
-        name: 'Riboflavin',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Potassium',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Phosphorus',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Pantothenic Acid',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Niacin',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Manganese',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Magnesium',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Iron',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Folic Acid',
-        units: [ 'µg', 'mcg' ]
-    },
-    {
-        name: 'Folate',
-        units: [ 'µg', 'mcg' ]
-    },
-    {
-        name: 'Copper',
-        units: [ 'mg' ]
-    },
-    {
-        name: 'Calcium',
-        units: [ 'mg' ]
-    },
-]
-
-main();
+main()
 
 async function main() {
-    for (let ingredientUrl of ingredients) {
+    let ingredients = YAML.load(fs.readFileSync('./ingredients.yaml', 'utf8'));
+    let nutrientConfigs = YAML.load(fs.readFileSync('./config.yaml', 'utf8'));
 
-        let body = (await curl.get(ingredientUrl)).body
-        body = body.replace(/<[^<]+>/g, "");
-        body = body.replace(/\n/g, "");
-        body = body.replace(/\n/g, "");
-        body = body.replace(/\\/g, "");
-        // body = body.replace(/\"/g, "'");
-        body = body.replace(/.+{"props":/g, "{\"props\":");
+    for (let ingredient of ingredients) {
+
+        if (ingredient.skip) {
+            continue
+        }
+
+        let body = (await curl.get(ingredient.url)).body
+        body = body.replace(/<[^<]+>/g, "")
+        body = body.replace(/\n/g, "")
+        body = body.replace(/\n/g, "")
+        body = body.replace(/\\/g, "")
+        body = body.replace(/.+{"props":/g, "{\"props\":")
         body = body.replace(/"globalAlert.*$/, "")
         body = body.replace(/,$/, "}}}")
-        // body = body.replace(/'/g, '"')
+        // p4(body)
 
-        let o = JSON.parse(body)
-        p4(o)
+        let wholeFoods = JSON.parse(body)
+        p4(wholeFoods)
+        let wf = wholeFoods.props.pageProps.data
 
-        console.log(o.props.pageProps.data.name + '\n')
-        console.log(o.props.pageProps.data.ingredients)
+        let data = []
+        data.push({ name: 'name', value: ingredient.name })
+        data.push({ name: 'fullName', value: mapName(wf.name) })
+        data.push({ name: 'url', value: ingredient.url })
+        data.push({ name: 'brand', value: mapBrand(wf.brand.name) })
+        data.push({ name: 'category', value: mapCategory(wf.categories.name) })
+
+        let ingredients = []
+        for (let ingredient of wf.ingredients) {
+            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*),(?=[^)]*\))/g, ";")
+            ingredient = ingredient.replace(/(?<=[^(]*\([^()]*)\.(?=[^)]*\))/g, "")
+            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*),(?=[^\]]*\])/g, ";")
+            ingredient = ingredient.replace(/(?<=[^\[]*\[[^\[\]]*)\.(?=[^\]]*\])/g, "")
+            for (let separatedIngredient of ingredient.split(/,/)) {
+                separatedIngredient = separatedIngredient.trim()
+                separatedIngredient = separatedIngredient.replace(/Ingredients: /, "")
+                separatedIngredient = separatedIngredient.replace(/\.$/, "")
+                ingredients.push(separatedIngredient)
+            }
+        }
+        data.push({ name: 'ingredients', value: ingredients })
+
+        let allergens = []
+        if (wf.allergens) {
+            for (let allergen of wf.allergens) {
+                allergens.push(allergen)
+            }
+        }
+        data.push({ name: 'allergens', value: allergens })
+
+        if (ingredient.consumptionUnit) {
+            data.push({ name: 'consumptionUnit', value: ingredient.consumptionUnit })
+            data.push({ name: 'consumptionGrams', value: ingredient.consumptionGrams })
+        } else {
+            data.push({ name: 'consumptionUnit', value: "grams" })
+            data.push({ name: 'consumptionGrams', value: "1" })
+        }
 
         let macroNutrientData = []
-        macroNutrientData.push({ name: "Serving Size", value: o.props.pageProps.data.servingInfo.secondaryServingSize, unit: o.props.pageProps.data.servingInfo.secondaryServingSizeUom })
+        let servingSize = wf.servingInfo.secondaryServingSize
+        if (wf.servingInfo.secondaryServingSizeUom !== 'g' && wf.servingInfo.secondaryServingSizeUom !== 'G') {
+            if (!ingredient['conversion_factor']) {
+                console.error('ERROR: No conversion factor exists for ' + ingredient.name)
+                process.exit(1)
+            }
+            servingSize *= ingredient.conversion_factor
+        }
+        data.push({ name: "servingSize", value: servingSize })
 
-        for (let macroNutrientSearchCriteria of macroNutrientSearchCriterias) {
-            addNutrient(macroNutrientSearchCriteria, o.props.pageProps.data.nutritionElements, macroNutrientData)
+        for (let nutrientConfig of nutrientConfigs) {
+
+            let wholeFoodsNutrient = _.find(wf.nutritionElements, { name: nutrientConfig.name })
+            if (!wholeFoodsNutrient || wholeFoodsNutrient.perServing == 0) {
+                continue
+            }
+
+            let amount = wholeFoodsNutrient.perServing
+
+            // If the whole foods nutrient uses a different unit of
+            // measure, verify that it's a known uom, and convert it
+            // if required.
+            if (wholeFoodsNutrient.uom !== nutrientConfig.unit) {
+                if (wholeFoodsNutrient.uom !== nutrientConfig.alternate_unit.unit) {
+                    console.error("ERROR: The Whole Foods nutrient's unit of measure is unknown!")
+                    process.exit(1)
+                }
+
+                if (nutrientConfig.alternate_unit.conversion_factor) {
+                    amount = amount * nutrientConfig.alternate_unit.conversion_factor
+                }
+            }
+
+            data.push({ name: nutrientConfig.alias, value: amount })
         }
 
-        let microNutrientData = []
-        for (let microNutrientSearchCriteria of microNutrientSearchCriterias) {
-            addNutrient(microNutrientSearchCriteria, o.props.pageProps.data.nutritionElements, microNutrientData)
+        // Add netcarbs to the macro nutrients
+        p4(data)
+        let carbs = _.find(data, { name: 'carbohydrates' })
+        let fiber = _.find(data, { name: 'fiber' })
+        if (carbs) {
+            data.push({ name: "netCarbs", value: carbs.value - fiber.value })
         }
 
-        console.log(print(macroNutrientData))
-        console.log(print(microNutrientData))
+        serialize(data)
+        console.log()
     }
 }
 
-function addNutrient(nutrientSearchCriteria, nutrients, data) {
-    let nutrient = _.find(nutrients, { name: nutrientSearchCriteria.name })
-    if (nutrient) {
-        let unit = nutrient.uom
-        if (!unit) {
-            unit = nutrientSearchCriteria.units[0]
+function mapCategory(category) {
+    category = category.replace(/u0026/g, '&')
+    return category
+}
+
+function mapName(name) {
+    name = name.replace(/u0026/g, '&')
+    return name
+}
+
+function mapBrand(brand) {
+    if (brand == '365 BY WFM') return 'Whole Foods 365'
+    if (brand == '365 Everyday Value®') return 'Whole Foods 365'
+    if (brand == 'PRODUCE') return 'Whole Foods 365'
+    if (brand == '365 by Whole Foods Market') return 'Whole Foods 365'
+    if (brand == 'Whole Foods Market') return 'Whole Foods 365'
+    if (brand == 'BHU FOODS') return 'Bhu Foods'
+    if (brand == 'siggi\'s') return 'Siggi\'s'
+    brand = brand.replace(/u0026/g, '&')
+    return brand
+}
+
+function serialize(data) {
+    p(data)
+
+    y(data, 'name', '', '- ')
+    y(data, 'brand')
+    y(data, 'fullName')
+    y(data, 'url')
+    y(data, 'totalCost')
+    y(data, 'totalGrams')
+    y(data, 'category')
+    y(data, 'ingredients')
+    y(data, 'allergens')
+
+    y(data, 'consumptionUnit')
+    y(data, 'consumptionGrams')
+
+    y(data, 'servingSize')
+    y(data, 'calories')
+    y(data, 'fat')
+    y(data, 'saturatedFat')
+    y(data, 'transFat')
+    y(data, 'polyunsaturatedFat')
+    y(data, 'monounsaturatedFat')
+    y(data, 'cholesterol')
+    y(data, 'sodium')
+    y(data, 'carbohydrates')
+    y(data, 'fiber')
+    y(data, 'sugar')
+    y(data, 'addedSugar')
+    y(data, 'sugarAlcohool')
+    y(data, 'netCarbs')
+    y(data, 'protein')
+
+    y(data, 'omega3')
+    y(data, 'zinc')
+    y(data, 'vitaminK')
+    y(data, 'vitaminE')
+    y(data, 'vitaminD')
+    y(data, 'vitaminC')
+    y(data, 'vitaminB6')
+    y(data, 'vitaminB12')
+    y(data, 'vitaminA')
+    y(data, 'thiamin')
+    y(data, 'selenium')
+    y(data, 'riboflavin')
+    y(data, 'potassium')
+    y(data, 'phosphorus')
+    y(data, 'pantothenicAcid')
+    y(data, 'niacin')
+    y(data, 'manganese')
+    y(data, 'magnesium')
+    y(data, 'iron')
+    y(data, 'folicAcid')
+    y(data, 'folate')
+    y(data, 'copper')
+    y(data, 'calcium')
+}
+
+function y(list, name, def, prefix) {
+    let pre = prefix || '  '
+
+    let stanza = _.find(list, { name: name })
+    if (stanza) {
+        if (Array.isArray(stanza.value)) {
+            console.log(pre + name + ':')
+            for (let item of stanza.value) {
+                console.log(pre + "- " + item)
+            }
+            return
         }
-        data.push({ name: nutrient.name, value: nutrient.perServing, unit: unit })
+        console.log(pre + name + ': ' + (stanza.value || def))
         return
     }
 
-    data.push({ name: nutrientSearchCriteria.name, value: 0, unit: nutrientSearchCriteria.units[0] })
-}
-
-function print(data) {
-    return table(data, [
-        {
-            name: 'name',
-            alias: 'Name',
-            width: -21
-        },
-        {
-            name: 'value',
-            alias: '#',
-            width: 11,
-        },
-        {
-            name: 'unit',
-            alias: 'Unit',
-            width: -5,
-        },
-    ])
+    console.log(pre + name + ': ' + (def || '0'))
 }
